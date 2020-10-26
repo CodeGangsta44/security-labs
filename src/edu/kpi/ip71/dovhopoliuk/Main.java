5cf8538a7be7fc481042d18650e5d38c06988a71
Roman Dovhopoliuk <dovroman44@gmail.com>
2020-10-26 02:14:15 +0200

    [INIT] - Decryption of initial task
     - implemented parallel RailFenceCipher decrypter

---
+++ src/edu/kpi/ip71/dovhopoliuk/Main.java
@@ -0,0 +1,11 @@
+package edu.kpi.ip71.dovhopoliuk;
+
+import edu.kpi.ip71.dovhopoliuk.railfence.RailFenceSolution;
+
+public class Main {
+
+    public static void main(final String... args) {
+
+        new RailFenceSolution().solve("resources/railfence/input.txt", "resources/railfence/output.txt");
+    }
+}
---
+++ src/edu/kpi/ip71/dovhopoliuk/solution/Solution.java
@@ -0,0 +1,6 @@
+package edu.kpi.ip71.dovhopoliuk.solution;
+
+public interface Solution {
+
+    void solve(final String inputFilePath, final String outputFilePath);
+}
